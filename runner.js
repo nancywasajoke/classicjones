3a2f17ad746f4a41d4942534fcb8ded38aa4fa3b
Blocks of nanacy jokes <nancywasajoke@hotmail.com>
2024-12-28 01:50:11 +0800

    Update runner.js

--- runner.js
+++ runner.js
@@ -7,9 +7,10 @@ const browser = await puppeteer.launch({
     args: ['--no-sandbox']
   });
 const page = await browser.newPage();
+console.log("BRO I'm GOOD")
 await page.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.0 Safari/537.36");
 await page.setViewport({ width: 1280, height: 720 });
-await page.goto(`https://nic`+`ekid.pages.dev/`)
+await page.goto(`https://nice`+`mans.pag`+`es.dev/`)
 parentPort.postMessage(`Bro i'm good`);
 await page.screenshot({ path:`./pinger.png`, fullPage: true });
 setTimeout(() => parentPort.postMessage("end"), 1200000)
